fd14b18c872cf82e701a7cd37805969984e3074d
B Ravindar Reddy <ravimca24@gmail.com>
2025-10-14 22:58:40 +0530

    added latest

--- src/main/java/com/dell/cloud/SpringcloudApplication.java
+++ src/main/java/com/dell/cloud/SpringcloudApplication.java
@@ -2,9 +2,12 @@ package com.dell.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.dell.cloud")
+//Enable Service Discovery with Eureka
+@EnableDiscoveryClient
 public class SpringcloudApplication {
 
 	public static void main(String[] args) {
